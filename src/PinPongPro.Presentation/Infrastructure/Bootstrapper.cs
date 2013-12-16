4bbd7cd38c5409572c1963b1287de5d9f1e45d4e
Saul Mayorquin <saul@acklenavenue.com>
2013-12-16 01:37:34 -0600

    moving to file first bootstraper task

--- src/PinPongPro.Presentation/Infrastructure/Bootstrapper.cs
+++ src/PinPongPro.Presentation/Infrastructure/Bootstrapper.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Routing;
-using AttributeRouting.Web.Http.WebHost;
-using AttributeRouting.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
-using PinPongPro.Presentation.Controllers;
 
 namespace PinPongPro.Presentation.Infrastructure
 {
@@ -42,36 +38,6 @@ namespace PinPongPro.Presentation.Infrastructure
         {
             GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-        }
-    }
-
-    public class ConfigureRoutes   : IBootstrapperTask
-    {
-        
-        #region IBootstrapperTask Members
-
-        public void Run()
-        {
-            var httpRouteCollection = GlobalConfiguration.Configuration.Routes;
-            httpRouteCollection.Clear();
-
-            var routeCollection = RouteTable.Routes;
-            routeCollection.Clear();
-
-            RegisterWebApi(httpRouteCollection);
-            RegisterMvc(routeCollection);
-        }
-
-        #endregion
-
-        void RegisterMvc(RouteCollection routes)
-        {
-            routes.MapAttributeRoutes(c => c.AddRoutesFromAssemblyOf<HomeController>());
-        }
-
-        void RegisterWebApi(HttpRouteCollection routes)
-        {
-            routes.MapHttpAttributeRoutes(c => c.AddRoutesFromAssembly(typeof(TournamentController).Assembly));
         }
     }
 }
---
+++ src/PinPongPro.Presentation/Infrastructure/ConfigureRoutes.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+using AttributeRouting.Web.Http.WebHost;
+using AttributeRouting.Web.Mvc;
+using PinPongPro.Presentation.Controllers;
+
+namespace PinPongPro.Presentation.Infrastructure
+{
+    public class ConfigureRoutes   : IBootstrapperTask
+    {
+        
+        #region IBootstrapperTask Members
+
+        public void Run()
+        {
+            var httpRouteCollection = GlobalConfiguration.Configuration.Routes;
+            httpRouteCollection.Clear();
+
+            var routeCollection = RouteTable.Routes;
+            routeCollection.Clear();
+
+            RegisterWebApi(httpRouteCollection);
+            RegisterMvc(routeCollection);
+        }
+
+        #endregion
+
+        void RegisterMvc(RouteCollection routes)
+        {
+            routes.MapAttributeRoutes(c => c.AddRoutesFromAssemblyOf<HomeController>());
+        }
+
+        void RegisterWebApi(HttpRouteCollection routes)
+        {
+            routes.MapHttpAttributeRoutes(c => c.AddRoutesFromAssembly(typeof(TournamentController).Assembly));
+        }
+    }
+}
